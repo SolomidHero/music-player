0427f06a6dd909ff5df27a90cd9b9f6f5e1a3c57
SolomidHero <dim.sad23@gmail.com>
2020-05-22 21:19:47 +0300

    Updates

---
+++ precache-manifest.24beb2882de62f92e44649ac6063d949.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7fec975c9d57a8ad5762a44cbd62506",
+    "url": "/music-player/index.html"
+  },
+  {
+    "revision": "377aa76422a6223183cd",
+    "url": "/music-player/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1701b2b75e3fbb0f5c42",
+    "url": "/music-player/static/css/main.22933951.chunk.css"
+  },
+  {
+    "revision": "377aa76422a6223183cd",
+    "url": "/music-player/static/js/2.f755ba1a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/music-player/static/js/2.f755ba1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1701b2b75e3fbb0f5c42",
+    "url": "/music-player/static/js/main.8dd6fb13.chunk.js"
+  },
+  {
+    "revision": "f375cb26c51a094314e9",
+    "url": "/music-player/static/js/runtime-main.51088be7.js"
+  },
+  {
+    "revision": "616dd4c04e106949644e218b18fe65db",
+    "url": "/music-player/static/media/Slow dancing in the dark.616dd4c0.mp3"
+  },
+  {
+    "revision": "0c873621bb12b131bcb76a17ecf504a3",
+    "url": "/music-player/static/media/Test Drive.0c873621.mp3"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/music-player/static/media/logo.25bf045c.svg"
+  }
+]);
